0699f1c22e75776315c9b34e4050713281375d19
Pascale Audet <audetpascale@gmail.com>
2021-12-24 19:13:47 -0500

    Upgrade to Gatsby 4

--- utils/gatsby-config.js
+++ utils/gatsby-config.js
@@ -6,5 +6,29 @@ module.exports = _.merge(config, {
     title: "Utilitaire de Pascale",
     description: "Les utilitaires de Pascale",
   },
-  plugins: ["gatsby-plugin-well-known"],
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-well-known",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 });
